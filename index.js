b24eca53a2814ee80d6ef7c0014bd869404c4c72
Sercan Şimşek <sercansimsek44@gmail.com>
2026-02-24 12:25:20 +0100

    Creatinga new post logic added

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-const submitBtn = document.querySelector(".submit-btn");
-const postTitle = document.querySelector("#post-title");
-const postBody = document.querySelector("#post-body");
+let postsArray = [];
+
+function renderPosts() {
+	let html = "";
+	for (let post of postsArray) {
+		html += `
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
+            <hr />
+        `;
+	}
+	document.getElementById("blog-list").innerHTML = html;
+}
 
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
 	.then((res) => res.json())
 	.then((data) => {
-		const postsArr = data.slice(0, 5);
-		let html = "";
-		for (let post of postsArr) {
-			html += `
-                <h3>${post.title}</h3>
-                <p>${post.body}</p>
-                <hr />
-            `;
-		}
-		document.getElementById("blog-list").innerHTML = html;
+		postsArray = data.slice(0, 5);
+		renderPosts();
 	});
 
 document.getElementById("new-post").addEventListener("submit", function (e) {
@@ -25,5 +27,19 @@ document.getElementById("new-post").addEventListener("submit", function (e) {
 		title: postTitle,
 		body: postBody,
 	};
-	console.log(data);
+
+	const options = {
+		method: "POST",
+		body: JSON.stringify(data),
+		headers: {
+			"Content-Type": "application/json",
+		},
+	};
+
+	fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+		.then((res) => res.json())
+		.then((post) => {
+			postsArray.unshift(post);
+			renderPosts();
+		});
 });
