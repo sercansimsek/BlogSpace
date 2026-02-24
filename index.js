647d1fcd93de958ca99eda6dbf830c84eaff8167
Sercan Şimşek <sercansimsek44@gmail.com>
2026-02-24 11:21:31 +0100

    Form elements added

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
-const list = document.querySelector(".list");
-
-const baseURL = "https://apis.scrimba.com/jsonplaceholder";
-
-fetch(`${baseURL}/posts`)
-  .then((response) => response.json())
-  .then((data) => {
-    let listHTML = "";
-
-    data.slice(0, 5).map((element) => {
-      const { title, body } = element;
-      listHTML += `
-            <li class="list-item">
-                <h3>${title}</h3>
-                <p>${body}</p>
-                <hr>
-            </li>
-        `;
-    });
-
-    list.innerHTML = listHTML;
-  });
-
-
- 
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+	.then((res) => res.json())
+	.then((data) => {
+		const postsArr = data.slice(0, 5);
+		let html = "";
+		for (let post of postsArr) {
+			html += `
+                <h3>${post.title}</h3>
+                <p>${post.body}</p>
+                <hr />
+            `;
+		}
+		document.getElementById("blog-list").innerHTML = html;
+	});
